8cc48396d33cea86da5f308726739102bb9a1e9d
AyaSapphire <ayasapphire@qq.com>
2025-09-01 01:58:56 +0800

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,20 +6,47 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: {
+				en: 'XDUCraft Docs',
+				zh: 'XDUCraft文档站'
+			},
+			favicon: '/images/favicon.png',
+			logo: {
+				src: './src/assets/logo.jpg'
+			},
+			tableOfContents: {
+				maxHeadingLevel: 3,
+				minHeadingLevel: 1
+			},			
+			description: '这是一个存放XDUCraft各种技术文档的网站',
+			social: [
+				{ icon: 'github', label: 'GitHub', href: 'https://github.com/ayasapphire/Docs-XDMC' },
+				{ icon: 'bilibili', label: 'Bilibili', href: 'https://space.bilibili.com/500642753'},
+				{ icon: 'QQ', label: 'QQ', href: 'http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=GF4Ej90Dz5BJpIEGFs4DjkHIouVGD0hR&authKey=hEzyZX6lIOTEk4jwmvZDSQZCD2MV24xiIexe%2BaI9yM9Meo%2Bu0P9TvBukIDFKH5hT&noverify=0&group_code=684941112'}
+			],
+			editLink: {
+				baseUrl: 'https://github.com/ayasapphire/Docs-XDMC/edit/main/'
+			},
 			sidebar: [
+				// 总览页面
 				{
-					label: 'Guides',
+					label: '总览',
 					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: '总览页面', slug: 'general/intro'}
 					],
 				},
+				// XDUCraft 入门图文教程
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'XDUCraft 入门图文教程',
+					autogenerate: { directory: 'join-server'}
 				},
+				// 整合包食用指南
+				{
+					label: '整合包食用指南',
+					items: [
+						{ label: '整合包食用指南', slug: 'modpack_tutorial'}
+					]
+				}
 			],
 		}),
 	],
